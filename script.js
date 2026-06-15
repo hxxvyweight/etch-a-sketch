dfee7029c0bbfbffa16b71192d3bc3949477a5f2
hxxvyweight <linuxtest923@gmail.com>
2026-06-15 14:33:07 +1000

    feat: Created blueprint for project

---
+++ script.js
@@ -0,0 +1,25 @@
+//Goal is to create a webpage with a 16x16 grid of square divs
+//Only Using JavaScript
+//Uses Flexbox to handle grid; Grid will have hover effect
+
+
+//Creates a resisable NxN grid with dimension specified by input
+function createDiv(num) {
+
+
+
+}
+
+//Clears all divs and content on Screen
+function clearDiv() {
+
+
+
+}
+
+//Handles user interaction.
+function userInteraction() {
+
+
+
+}
