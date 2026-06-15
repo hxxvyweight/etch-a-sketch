61e29af4d529c004d03d63447592d85a4bc65e8f
hxxvyweight <linuxtest923@gmail.com>
2026-06-15 15:53:35 +1000

    feat: Create NxN Grid Generation based on user input

--- script.js
+++ script.js
@@ -8,10 +8,15 @@ const container = document.createElement("div");
 
 //Creates a resisable NxN grid with dimension specified by input
 function createDiv(num) {
-
+    
+    //Create NxN Grid generation based on user input
+    const totalSquare = num * num;
+    
     for (let i = 0; i < num; i++) {
         const square = document.createElement("div");
         square.id = "square-div";
+
+        container.appendChild(square);
     }
 
 
