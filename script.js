2e4cc17cf6f0296a28f67810a113ff23a345a57b
hxxvyweight <linuxtest923@gmail.com>
2026-06-15 16:09:51 +1000

    feat: added looped prompt to userInteraction function, handles errors or unknown inputs.

--- script.js
+++ script.js
@@ -4,15 +4,16 @@
 
 
 const container = document.createElement("div");
+document.body.append(container);
 
 
 //Creates a resisable NxN grid with dimension specified by input
-function createDiv(num) {
+function createDiv(userInput) {
     
     //Create NxN Grid generation based on user input
-    const totalSquare = num * num;
+    const totalSquare = userInput * userInput;
     
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < userInput; i++) {
         const square = document.createElement("div");
         square.id = "square-div";
 
@@ -24,7 +25,7 @@ function createDiv(num) {
 }
 
 //Clears all divs and content on Screen
-function clearDiv() {
+function clearDiv(clearCondition) {
 
 
 
@@ -32,7 +33,35 @@ function clearDiv() {
 
 //Handles user interaction.
 function userInteraction() {
+    
+    while (true) {
+
+        const input = prompt("Please Type a Number: ");
+
+        if (input === null) {
+            alert("Action Cancelled by User");
+            return;
+        }
+
+        if ( isNaN(input) || input.trim() === "" || Number(input) < 0 ) {
+
+            alert("Invalid input, please enter a positive number!");
+            continue;
+
+        }
+
+        const validNumber = Number(input);
+        console.log("Success! Your number is:", validNumber);
+
+        break;
+    
+    }
+    
 
 
 
-}
+
+
+    }
+
+
